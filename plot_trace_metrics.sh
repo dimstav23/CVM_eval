43cebe2f8d6d934858790edf187296f118826c0e
dimstav23 <dimstav23@gmail.com>
2025-08-06 07:37:04 +0000

    update plotting scripts for perf trace, add a patch and automate the plot generation
    Signed-off-by: dimstav23 <dimstav23@gmail.com>

--- plot_trace_metrics.sh
+++ plot_trace_metrics.sh
@@ -4,7 +4,8 @@ set -euxo pipefail
 PLOT_DIR="trace/plotting_scripts"
 
 cd $PLOT_DIR
-python3 plot_cpu.py
-python3 plot_memory.py
-python3 plot_network_io.py
-python3 plot_storage_io.py
+python3 plot_cpu.py --redis --fio --network
+python3 plot_memory.py --redis --fio --network
+python3 plot_network_io.py --redis --fio --network
+python3 plot_storage_io.py --redis --fio --network
+python3 plot_interrupts.py --redis --fio --network
